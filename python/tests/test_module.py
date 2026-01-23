b73d6914e18c55e1e2599e0440bf8bb403e69363
Stephen Shen <zs435@cornell.edu>
2026-01-23 11:02:13 -0500

    explicitly close file

--- python/tests/test_module.py
+++ python/tests/test_module.py
@@ -9,9 +9,11 @@ TEST_DIR = os.path.join(os.path.dirname(__file__))
 class TestECHO(unittest.TestCase):
     def test_parse_flat_payload(self):
         # read data/test_flat.json into payload dict
-        payload = json.load(open(TEST_DIR + "/data/test_flat_basic.json", "r"))
-        flat_ECHO(**payload)
+        with open(TEST_DIR + "/data/test_flat_basic.json", "r") as f:
+            payload = json.load(f)
+            flat_ECHO(**payload)
 
-    def test_parse_nested_payload(self):    
-        payload = json.load(open(TEST_DIR + "/data/test_nested_basic.json", "r"))
-        nested_ECHO(**payload)
+    def test_parse_nested_payload(self): 
+        with open(TEST_DIR + "/data/test_nested_basic.json", "r") as f:
+            payload = json.load(f)
+            nested_ECHO(**payload)
